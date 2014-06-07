66de6e0ed4d6cda68ff7014b68263d0ceada36eb
Kevin Miller <kemiller2002@gmail.com>
2014-06-07 12:05:41 -0400

    Added Real time Dashboard

---
+++ RealTimeDashboard/Dashboard/Models/ProcessorHub.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Dashboard.Models
+{
+    public class ProcessorHub : Hub
+    {
+        public void SendProcessor(int percentage)
+        {
+            Clients.All.broadcastProcessorStats(percentage);
+        }
+
+        public static void SendProcessorUpdate(int percentage)
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<ProcessorHub>();
+
+            context.Clients.All.broadcastProcessorStats(percentage);
+        }
+
+    }
+}
---
+++ RealTimeDashboard/Dashboard/Models/ProcessorMonitor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Diagnostics;
+
+namespace Dashboard.Models
+{
+
+
+
+    public class ProcessorMonitor
+    {
+
+        PerformanceCounter _processCounter = null;
+        private ProcessorMonitor() 
+        {
+            _processCounter = new PerformanceCounter();
+
+            _processCounter.CategoryName = "Processor";
+            _processCounter.CounterName = "% Processor Time";
+            _processCounter.InstanceName = "_Total";
+        }
+
+        static ProcessorMonitor _processorMonitor = null;
+
+        public float ProcessorUsage
+        {
+            get { return _processCounter.NextValue(); }
+        }
+
+        public static ProcessorMonitor GetProcessorMonitor () {
+            if (_processorMonitor == null)
+            {
+                _processorMonitor = new ProcessorMonitor();
+            }
+
+            return _processorMonitor;
+        }
+    }
+}
