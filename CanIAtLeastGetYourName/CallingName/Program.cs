cbe34727ea47cf5febbbde0ef8c777355855bfe5
Kevin Miller <kemiller2002@gmail.com>
2014-07-13 10:47:23 -0400

    Added reflection

--- CanIAtLeastGetYourName/CallingName/Program.cs
+++ CanIAtLeastGetYourName/CallingName/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.CompilerServices;
+using System.Reflection;
 
 namespace StructuredSight.CallerExample
 {
@@ -17,7 +18,26 @@ namespace StructuredSight.CallerExample
             
             prog.DoApplicationThings();
 
+            prog.ReflectionCall();
+
+            Console.ReadLine();
         }
+
+
+        void ReflectionCall()
+        {
+            var typeInfo = typeof(StructuredSight.CallerExample.Logging.Logger);
+
+            var method = typeInfo.GetMethod("Log");
+
+            /*
+             * method.Invoke(null, new[] { "Reflection Method" });  
+             * //Runtime Error!  Default parameters don't work with reflection.  They have to specified!
+             */
+
+            method.Invoke(null, new object [] { "Reflection Method", "My Method", "My File Path", 0 });  
+        }
+
 
         void DoApplicationThings()
         {
@@ -25,10 +45,7 @@ namespace StructuredSight.CallerExample
             var mLogic = new MoreBusinessLogic();
 
             mLogic.DoApplicationThings();
-
-            Console.ReadLine();
         }
-        
 
 
 
