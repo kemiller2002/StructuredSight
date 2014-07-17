399cd53e2bd0d81d39662e1e4b1104ae67107ea6
kemiller2002 <kemiller2002@gmail.com>
2014-07-17 15:29:12 -0400

    Sparkline now works. working on bar chart.

--- D3/Templates/Scripts/sparkline.ts
+++ D3/Templates/Scripts/sparkline.ts
@@ -1,17 +1,29 @@
-﻿interface ICalculate {
-    (number): number; 
-}
-interface IBounds {
+var d3: any;
+
+
+interface ICalculate {
+     (number): number;
+ }
+
+ interface IRender {
+     (number) : string;
+ }
+
+ interface IBounds extends ICalculate {
     domain(upperAndLowerBound: number[]): IBounds;
     range(upperAndLowerBound: number[]): IBounds;
-    ICalculate;
 }
 
-interface IScale {
+ interface IScale {
+     scale: IScale;
     linear(): IBounds;
+ }
+
+interface  IData {
+    enter(): IHtml;
 }
 
-interface IGraphFunctions {
+interface IGraphFunctions extends  IRender {
     x(f: (d: number, i: number) => number): IGraphFunctions ;
     y(f: (d: number, i: number) => number): IGraphFunctions ;
 }
@@ -20,22 +32,48 @@ interface IGraphType {
     line (): IGraphFunctions;
 }
 
-interface IGraphingUtility extends IScale {
-    select(id: string): IGraphingUtility;
+interface  IHtml {
     append(htmlNode: string): IGraphingUtility;
     attr(attribute: string, value: string): IGraphingUtility;
+}
+
+interface IGraphingUtility extends IScale, IHtml {
+    select(id: string): IGraphingUtility;
+    selectAll(id:string) : IHtmlCollection;
     svg: IGraphType;
+    max(data : number[]): number;
 }
 
-function openGraphUtility(grapher: IGraphingUtility) {
-
-    function getGraph(wrapper: string) {
-        var g = grapher.select(wrapper).append("svg:svg").append("width:100%").append("width:100%");
-        var x = g.linear().domain([0, 50]).range([0, 50]);
-        var y = g.linear().domain([0, 50]).range([0, 50]);
-
-        var svg = grapher.svg.line().x((d, i) => { return x(d); }).y((d, i) => return y(i););
-
-    }
+interface IHtmlCollection {
+    data(d: any[]) :IHtmlCollection;
+    enter(): IHtmlCollection;
+    append(node: string): IHtmlCollection;
+    attr(attribute:string, value:string) : IHtmlCollection;
 }
 
+function openSparkGraphUtility(grapher: IGraphingUtility) : (string) => void {
+
+    return (wrapper: string) => {
+        var g = grapher.select("#" + wrapper).append("svg:svg").attr("width", "100%").attr("height", "100%");
+        var x = grapher.scale.linear().domain([0, 50]).range([0, 100]);
+        var y = grapher.scale.linear().domain([0, 50]).range([0, 100]);
+
+        var svg = d3.svg.line().x((d, i) => { return x(i); }).y((d, i) => { return y(d); });
+      
+        return (data: number[]) => { g.append("svg:path").attr("d", svg(data)); };
+    };
+}
+
+function openBarGraphUtility(grapher: IGraphingUtility): (string, number) => void {
+
+    return (wrapper: string, data:number[]) => {
+        var g = grapher.select("#" + wrapper).append("svg:svg").attr("width", "100%").attr("height", "100%");
+
+        g.selectAll("g").
+        
+        var x = grapher.scale.linear().domain([0, grapher.max(data)]).range([0, 100]);
+        //var y = grapher.scale.linear().domain([0, 50]).range([0, 100]);
+
+        var svg = d3.svg.line().x((d, i) => { return x(i); }).y((d, i) => { return y(d); });
+    };
+}
