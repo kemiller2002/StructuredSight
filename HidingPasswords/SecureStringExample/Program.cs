8798c58fea0c5bc9710b6b99f37b7c78657a7688
Kevin Miller <kemiller2002@gmail.com>
2014-08-02 22:11:31 -0400

    updated unsafe

--- HidingPasswords/SecureStringExample/Program.cs
+++ HidingPasswords/SecureStringExample/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Security;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace SecureStringExample
 {
@@ -12,15 +14,46 @@ namespace SecureStringExample
         static void Main(string[] args)
         {
             Console.Write("Enter Passphrase: ");
-
             using (var passphrase = GetPassphraseFromConsole())
             {
+                var arPass = new char[passphrase.Length];
 
+                Console.WriteLine("Your password:");
 
+                RuntimeHelpers.ExecuteCodeWithGuaranteedCleanup(
+                    (uData) =>
+                    {
+                        unsafe
+                        {
+                            var handle = System.Runtime.InteropServices.GCHandle.Alloc(arPass, System.Runtime.InteropServices.GCHandleType.Pinned);
 
+                            System.IntPtr ptr = System.IntPtr.Zero;
 
+                            ptr = Marshal.SecureStringToBSTR(passphrase);
 
+                            char* ptrPassword = (char*)ptr;
 
+                            char* ptrArPass = (char*)handle.AddrOfPinnedObject();
+
+                            for (var ii = 0; ii < arPass.Length; ii++)
+                            {
+                                ptrArPass[ii] = ptrPassword[ii];
+                            }
+
+                            Console.WriteLine(arPass);
+                        }
+
+                    },
+
+                    (uData, ifExceptionThrown) =>
+                    {
+                        for (var ii = 0; ii < arPass.Length; ii++)
+                        {
+                            arPass[ii] = '\0';
+                        }
+                    },
+                    null
+                    );
             }
 
         }
@@ -35,7 +68,10 @@ namespace SecureStringExample
             while (key.Key != ConsoleKey.Enter)
             {
                 password.AppendChar(key.KeyChar);
+                key = Console.ReadKey(true);
             }
+            
+            password.MakeReadOnly();
 
             return password;
         }
