c8930c594dbbf8e0f49e47b62b0605f420de1ac5
Kevin Miller <kemiller2002@gmail.com>
2014-08-02 21:05:28 -0400

    working on secure string example

---
+++ HidingPasswords/SecureStringExample/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security;
+
+namespace SecureStringExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Passphrase: ");
+
+            using (var passphrase = GetPassphraseFromConsole())
+            {
+
+
+
+
+
+
+            }
+
+        }
+
+
+        static SecureString GetPassphraseFromConsole()
+        {
+            var password = new SecureString();
+
+            var key = Console.ReadKey(true);
+
+            while (key.Key != ConsoleKey.Enter)
+            {
+                password.AppendChar(key.KeyChar);
+            }
+
+            return password;
+        }
+
+
+
+    }
+}
