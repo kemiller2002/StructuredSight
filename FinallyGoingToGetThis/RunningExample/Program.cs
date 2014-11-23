d5a24d9733e326d705f82f138f23ed85d2a827ac
Kevin Miller <kemiller2002@gmail.com>
2014-11-23 09:16:21 -0500

    finally execute

---
+++ FinallyGoingToGetThis/RunningExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunningExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new Exception("This is a normal exception.");
+            }
+            finally
+            {
+                Console.WriteLine("Finally block executed.");
+            }
+        }
+
+    }
+}
