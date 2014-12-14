ea3b8f3faed60b15e647f687fd3669e9307e937e
Kevin Miller <kemiller2002@gmail.com>
2014-12-14 16:37:06 -0500

    made shorter

--- AutomaticallyCreateNuget/CreateNugetExample/CreateNugetPackage.ps1
+++ AutomaticallyCreateNuget/CreateNugetExample/CreateNugetPackage.ps1
@@ -11,23 +11,27 @@ $nuget = "C:\Utilities\Nuget\NuGet.exe"
 
 [xml]$nuspecContent = switch ([System.IO.File]::Exists($nuspecFilePath)) 
 {
-    $false  {  Write-Host "nuspec file not found at: $projectPath. Creating"
-               & $nuget spec $projectPath | Write-Host 
-               [xml]$file = Get-Content $nuspecFilePath
+  $false { 
+           Write-Host "nuspec file not found at: $projectPath. Creating"
+           & $nuget spec $projectPath | Write-Host 
+           [xml]$file = Get-Content $nuspecFilePath
 
-               $file.package.metadata.id = $targetName
-               
-               $file.package.metadata.RemoveChild($file.package.metadata.SelectSingleNode("licenseUrl")) | Out-Null
-               $file.package.metadata.RemoveChild($file.package.metadata.SelectSingleNode("projectUrl")) | Out-Null 
-               $file.package.metadata.RemoveChild($file.package.metadata.SelectSingleNode("iconUrl")) | Out-Null 
+           $metaData = $file.package.metadata 
+           $metaData.id = $targetName
+              
+           $metaData.RemoveChild($metaData.SelectSingleNode("licenseUrl")) | Out-Null
+           $metaData.RemoveChild($metaData.SelectSingleNode("projectUrl")) | Out-Null 
+           $metaData.RemoveChild($metaData.SelectSingleNode("iconUrl")) | Out-Null 
 
-               $file
-            }
+           $file
+          }
     $true   {Get-Content $nuspecFilePath}
     
 }
 
-$assemblyVersion = [System.Reflection.Assembly]::LoadFile($targetPath).GetName().Version.ToString()
+$assembly = [System.Reflection.Assembly]::LoadFile($targetPath)
+
+$assemblyVersion = $assembly.GetName().Version.ToString()
 
 Write-Host "Setting assembly version to: $assemblyVersion"
 
