d233773546c05aa899fe2fbf43ab3cda8d3b07d6
Kevin Miller <kemiller2002@gmail.com>
2015-01-31 20:47:35 -0500

    adding table valued parameters

--- CompileTimeStoredProcedures/CompileTimeStoredProcedures/StoredProcedures.cs
+++ CompileTimeStoredProcedures/CompileTimeStoredProcedures/StoredProcedures.cs
@@ -10,17 +10,38 @@ namespace glbl
 {
 
 
-  [Query("SelectProducts")]
-  public class SelectProducts
+  [Query("InsertUpdateLogEntry")]
+  public partial class InsertUpdateLogEntry
   {
 
-  	public SelectProducts (
-		
+  	public InsertUpdateLogEntry (
+		int Major,
+int Minor,
+int Patch,
+string ProductName
 	)
 	{
-		
+		this.Major = Major;
+this.Minor = Minor;
+this.Patch = Patch;
+this.ProductName = ProductName;
 	}
 	
+
+		private readonly int _Major;
+private readonly int _Minor;
+private readonly int _Patch;
+private readonly string _ProductName;
+
+		[QueryParameter("@Major")] 
+ public int Major{get {return _Major;}}
+[QueryParameter("@Minor")] 
+ public int Minor{get {return _Minor;}}
+[QueryParameter("@Patch")] 
+ public int Patch{get {return _Patch;}}
+[QueryParameter("@ProductName")] 
+ public string ProductName{get {return _ProductName;}}
+
   }
 
 }
