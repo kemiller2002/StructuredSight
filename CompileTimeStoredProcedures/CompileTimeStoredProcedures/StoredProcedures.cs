b1fab09240bd54273aa21269134ba645a05cb6e8
Kevin Miller <kemiller2002@gmail.com>
2015-03-14 20:25:53 -0400

    updates to compile time stored procedures

--- CompileTimeStoredProcedures/CompileTimeStoredProcedures/StoredProcedures.cs
+++ CompileTimeStoredProcedures/CompileTimeStoredProcedures/StoredProcedures.cs
@@ -6,55 +6,90 @@
 
 
 
-namespace glbl
-{
 
-    public partial class CategoryTableType
-    {
-        public int? CategoryID { get; set; }
-        public string CategoryName { get; set; }
-    }
+/*Start Table Valued Columns*/ 
 
-		
+		namespace CompileTimeStoredProcedures.TableValuedParameter.dbo
+		{ 
+			public partial class CategoryTableType {
+				public int? CategoryID{get;set;}
+public string CategoryName{get;set;}
+			}
+
+		}
+
+		/*End Table Valued Columns*/
+ 
 
 
 
+  namespace CompileTimeStoredProcedures.Query.dbo
+  { 
 
-  [Query("dbo.InsertUpdateLogEntry")]
-  public partial class InsertUpdateLogEntry
-{
-
-  	public InsertUpdateLogEntry (
-		int major,
+	  public partial class InsertUpdateLogEntry : IDbQuery {
+	
+	  	public InsertUpdateLogEntry (
+			int major,
 int minor,
 int patch,
 string productName
-	)
-	{
-		Major = major;
+		)
+		{
+			Major = major;
 Minor = minor;
 Patch = patch;
 ProductName = productName;
-	}
-	
-
-		private readonly int _major;
+		}
+		
+			public String Query {get{return "InsertUpdateLogEntry";}}
+			private readonly int _major;
 private readonly int _minor;
 private readonly int _patch;
 private readonly string _productName;
-
-		[QueryColumn("@Major")] 
+	
+			[Parameter("@Major")] 
  public int Major{get {return _major;}}
-[QueryColumn("@Minor")] 
+[Parameter("@Minor")] 
  public int Minor{get {return _minor;}}
-[QueryColumn("@Patch")] 
+[Parameter("@Patch")] 
  public int Patch{get {return _patch;}}
-[QueryColumn("@ProductName")] 
+[Parameter("@ProductName")] 
  public string ProductName{get {return _productName;}}
-
+	
+		}
 	}
 
+	namespace CompileTimeStoredProcedures.Result.dbo
+	{ 
+		public partial class InsertUpdateLogEntry_Result {
+		public   {get;set;}		}
+	}
 
+  namespace CompileTimeStoredProcedures.Query.dbo
+  { 
+
+	  public partial class SelectLogEntry : IDbQuery {
+	
+	  	public SelectLogEntry (
+			
+		)
+		{
+			
+		}
+		
+			public String Query {get{return "SelectLogEntry";}}
+			
+	
+			
+	
+		}
+	}
+
+	namespace CompileTimeStoredProcedures.Result.dbo
+	{ 
+		public partial class SelectLogEntry_Result 
+        {
+		public   {get;set;}		
+        }
 }
-
 
