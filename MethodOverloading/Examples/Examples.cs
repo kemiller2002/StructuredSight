833dd735638204da5a118b02156ee1925f301dfb
Kevin Miller <kemiller2002@gmail.com>
2015-03-30 07:26:43 -0400

    method overloading

---
+++ MethodOverloading/Examples/Examples.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Examples
+{
+    [TestClass]
+    public class Examples
+    {
+        [TestMethod]
+        public void ShowParentExample()
+        {
+            var parent = new MethodOverloading.Parent ();
+
+            System.Diagnostics.Debug.WriteLine(parent.FormatMessage("Hello World"));
+        }
+
+
+        [TestMethod]
+        public void ShowChildExample()
+        {
+            var child = new MethodOverloading.Child();
+
+            System.Diagnostics.Debug.WriteLine(child.FormatMessage("Hello World"));
+        }
+
+        [TestMethod]
+        public void ShowChildExampleDeclaredAsParent()
+        {
+            MethodOverloading.Parent typeParent = new MethodOverloading.Child();
+            MethodOverloading.Child typeChild = (MethodOverloading.Child)typeParent;
+
+
+            System.Diagnostics.Debug.WriteLine("Variable type parent");
+            System.Diagnostics.Debug.WriteLine(typeParent.FormatMessage("Hello World"));
+
+
+            System.Diagnostics.Debug.WriteLine("");
+            System.Diagnostics.Debug.WriteLine("Variable type child");
+            System.Diagnostics.Debug.WriteLine(typeChild.FormatMessage("Hello World"));
+
+        
+        }
+
+
+        [TestMethod]
+        public void ShowParentAppendExample()
+        {
+            var parent = new MethodOverloading.Parent();
+
+            System.Diagnostics.Debug.WriteLine(parent.AppendAndFormat("Hello World", "!"));
+        }
+
+
+        [TestMethod]
+        public void ShowChildAppendExample()
+        {
+            var child = new MethodOverloading.Child();
+
+            System.Diagnostics.Debug.WriteLine(child.AppendAndFormat("Hello World", "!"));
+        }
+
+
+
+        /*equal overloads*/
+
+        [TestMethod]
+        public void EqualOverloadString()
+        {
+            var equalOverload = new MethodOverloading.EqualOverloads();
+
+            System.Diagnostics.Debug.WriteLine(equalOverload.FormatMessage("Hello World"));
+        }
+
+        [TestMethod]
+        public void EqualOverloadObject()
+        {
+            var equalOverload = new MethodOverloading.EqualOverloads();
+
+            System.Diagnostics.Debug.WriteLine(equalOverload.FormatMessage((object)"Hello World"));
+        }
+
+
+    }
+}
---
+++ MethodOverloading/MethodOverloading/Classes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    public class Parent
+    {
+        public string FormatMessage(string message)
+        {
+            return "This is the parent: " + message;
+        }
+
+        public string AppendAndFormat(string message, string append)
+        {
+            return FormatMessage(message + append);
+        }
+        
+    }
+
+    public class Child : Parent
+    {
+        public string FormatMessage(object message)
+        {
+            return "This is the child: " + message;
+        }
+    }
+
+
+    public class EqualOverloads
+    {
+
+        public string FormatMessage(string message)
+        {
+            return "This is the string: " + message;
+        }
+
+        public string FormatMessage(object message)
+        {
+            return "This is the object: " + message;
+        }
+    }
+
+
+
+
+}
