f200677937a5ee687b48c2c810f9af90427b1113
Kevin Miller <kemiller2002@gmail.com>
2016-02-21 11:23:09 -0500

    add override

---
+++ NewConsoleOutput/ConsoleOutputExample/DbWriter.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.Text;
+
+namespace ConsoleOutputExample
+{
+    public class DbWriter : TextWriter
+    {
+        public override Encoding Encoding { get; }
+
+
+    }
+
+}
