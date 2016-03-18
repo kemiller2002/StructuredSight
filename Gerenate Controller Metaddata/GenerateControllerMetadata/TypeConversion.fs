c064d76ddee37656821aa361ae117f25afb6e342
Kevin Miller <kemiller2002@gmail.com>
2016-03-18 18:02:37 -0400

    initial add

---
+++ Gerenate Controller Metaddata/GenerateControllerMetadata/TypeConversion.fs
@@ -0,0 +1,17 @@
+﻿namespace StructuredSight.Web.Api
+open System
+open System.Reflection
+open StructuredSight
+open System.Web
+
+
+module TypeConversion  = 
+    let DiscoverTypes (mi:Method) = 
+         seq {
+            yield! mi.Parameters |> Seq.map (fun x-> x.Variable.ObjectType.Name)
+            if mi.ReturnType.IsSome then yield mi.ReturnType.Value.Name
+         }
+    
+    let ConvertTypes(name:Name) = 
+        let oType = Type.GetType(sprintf "%s.%s" (if name.Namespace.IsSome then name.Namespace.Value else "System") name.Name)
+        oType.GetProperties()
