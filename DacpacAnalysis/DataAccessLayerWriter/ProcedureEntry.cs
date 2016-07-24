7f755132d418c50dfbd5170897272af553998cac
Kevin Miller <kemiller2002@gmail.com>
2016-07-24 14:43:43 -0400

    Updating code generator

---
+++ DacpacAnalysis/DataAccessLayerWriter/ProcedureEntry.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayerWriter
+{
+    public static class ProcedureEntry
+    {
+
+
+        static string CreateParameterEntry(Field parameter)
+        {
+            return
+                $@"
+            if ({parameter.Name}.HasValue)
+            {{
+                {{
+                    var parameter = new SqlParameter
+                {{
+                    Value = id,
+                    ParameterName = ""@{parameter.Name}"",
+                }};
+
+                    command.Parameters.Add(parameter);
+                }}
+            }}";
+        }
+
+
+
+        public static string Create(string procedureNamespace, string name, IEnumerable<Field> parameters)
+        {
+
+            var codeParameters = parameters.Select(CreateParameterEntry);
+
+            var parameterEntry = String.Join(System.Environment.NewLine, codeParameters);
+
+            return $@"
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Dynamic;
+using System.Net.Configuration;
+
+
+namespace {procedureNamespace}
+{{
+
+    public class {name}
+    {{
+
+        readonly SqlConnection _connection;
+
+        public SelectEmployees(SqlConnection connection)
+        {{
+            _connection = connection;
+        }}
+
+        public IEnumerable<dynamic> Execute(int? id)
+        {{
+            var command = new SqlCommand();
+            command.Connection = _connection;
+            command.CommandText = ""{name}"";
+
+            /*if (id.HasValue)
+            {{
+                var parameter = new SqlParameter
+                {{
+                    Value = id,
+                    ParameterName = ""@id"",
+                }};
+
+                command.Parameters.Add(parameter);
+            }}*/
+
+
+            {parameterEntry}
+
+
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {{
+                dynamic record = new ExpandoObject();
+
+                for (var ii = 0; ii < reader.FieldCount; ii++)
+                {{
+                    record[reader.GetName(ii)] = reader[ii];
+                }}
+
+                yield return record;
+            }}
+        }}
+
+
+    }}
+
+}}
+
+";
+    }
+
+
+
+
+
+
+    }
+}
--- DacpacAnalysis/DataAccessLayerWriter/Program.cs
+++ DacpacAnalysis/DataAccessLayerWriter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -6,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO.Compression;
 using System.Security.Cryptography;
+using System.Text.RegularExpressions;
 using System.Xml;
 
 namespace DataAccessLayerWriter
@@ -17,13 +19,16 @@ namespace DataAccessLayerWriter
         {
 
             var writer = new Writer();
-            writer.Write();
+            writer.Write(
+                @"Z:\Users\kemiller2002\GitHub\StructuredSight\DacpacAnalysis\Database1\Database1\bin\Debug\Database1.dacpac",
+                "C:\\temp\\"
+                );
         }
 
-        public bool Write()
+        public bool Write(string inputFile, string outputFolder)
         {
-            var archive = ZipFile.Open(
-                @"Z:\Users\kemiller2002\GitHub\StructuredSight\DacpacAnalysis\Database1\Database1\bin\Debug\Database1.dacpac",
+            var archive = ZipFile.Open(inputFile
+                ,
                     System.IO.Compression.ZipArchiveMode.Read);
 
             using (var stream = archive.Entries.First(x => x.Name.Equals("model.xml")).Open())
@@ -34,6 +39,8 @@ namespace DataAccessLayerWriter
                 var manager = new XmlNamespaceManager(document.NameTable);
                 manager.AddNamespace("d", document.DocumentElement.NamespaceURI);
                 var nodes = document.SelectNodes("d:DataSchemaModel/d:Model/d:Element", manager);
+
+                Console.WriteLine(nodes.Cast<XmlNode>().Where(n => n.Attributes["Type"].Value.Equals("SqlProcedure")).Count());
 
                 var code = nodes.Cast<XmlNode>()
                      .Where(n => n.Attributes["Type"].Value.Equals("SqlProcedure"))
@@ -58,16 +65,17 @@ namespace DataAccessLayerWriter
                 ? typeNode.Attributes["Name"].Value.RemoveSquareBrackets()
                 : "";
 
+            var allowsNull = node.SelectSingleNode("d:Element/d:Property[@Name='DefaultExpressionScript']", manager) != null;
 
             return new Field
             {
                 Name = name,
-                Type = type,
-                AllowsNull = true
+                Type = LookupDataType(type),
+                AllowsNull = allowsNull
             };
         }
         
-        public string ParseProcedure(XmlNode node, XmlNamespaceManager manager)
+        public Tuple<string,string> ParseProcedure(XmlNode node, XmlNamespaceManager manager)
         {
             var nameAttributeValue = node.Attributes["Name"].Value;
             var nameAttributeValueParts = nameAttributeValue.Split('.');
@@ -78,12 +86,81 @@ namespace DataAccessLayerWriter
             var parameters = node.SelectNodes("d:Relationship[@Name='Parameters']/d:Entry", manager)
                 .Cast<XmlNode>().Select(n => CreateParameterEntry(n, manager));
 
-            return parameters.Count().ToString();
+            return new Tuple<string, string>($"{procedureNamespace}{procedureName}", 
+                    ProcedureEntry.Create(procedureNamespace, procedureName, parameters));
         }
 
+        public string LookupDataType(string input)
+        {
+           switch (input)  
+            {
+                case "bigint": return "Int64";
 
+                case "binary": return "Byte[]";
 
+                case "bit": return "Boolean";
 
+                case "char": return "String";
+
+                case "date": return "DateTime";
+
+                case "Date": return "datetime";
+
+                case "datetime2": return "DateTime";
+
+                case "datetimeoffset": return "DateTimeOffset";
+
+                case "decimal": return "Decimal";
+
+                case "FILESTREAM": return "Byte[]";
+
+                case "float": return "Double";
+
+                case "image": return "Byte[]";
+
+                case "int": return "Int32";
+
+                case "money": return "Decimal";
+
+                case "nchar": return "String";
+
+                case "ntext": return "String";
+
+                case "numeric": return "Decimal";
+
+                case "nvarchar": return "String";
+
+                case "real": return "Single";
+
+                case "rowversion": return "Byte[]";
+
+                case "smalldatetime": return "DateTime";
+
+                case "smallint": return "Int16";
+
+                case "smallmoney": return "Decimal";
+
+                case "sql_variant": return "Object";
+
+                case "text": return "String";
+
+                case "time": return "TimeSpan";
+
+                case "timestamp": return "Byte[]";
+
+                case "tinyint": return "Byte";
+
+                case "uniqueidentifier": return "Guid";
+
+                case "varbinary": return "Byte[]";
+
+                case "varchar": return "String";
+
+                case "xml": return "Xml";
+            }
+
+            throw new Exception($"Type not found: {input}");
+        }
 
     }
 }
