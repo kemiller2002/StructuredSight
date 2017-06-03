d5ddde987b89064bc2e854ccfcf0f1d6039040a9
Kevin Miller <kemiller2002@gmail.com>
2017-06-03 11:48:27 -0400

    Pattern Matching.

--- PatternMatchingQuirks_Standard/ExamplesDotNetFramework/PatternMatchingExamples.cs
+++ PatternMatchingQuirks_Standard/ExamplesDotNetFramework/PatternMatchingExamples.cs
@@ -8,5 +8,27 @@ namespace ExamplesDotNetFramework
 {
     class PatternMatchingExamples
     {
+        static void NullExample ()
+        {
+            string jennysNumber = "867-5309";
+
+            switch (jennysNumber)
+            {
+
+                case null:
+                    Console.WriteLine("There is no Jenny's Number");
+                    break;
+                case string s:
+                    Console.WriteLine("Well at least it is a string");
+                    break;
+
+
+            }
+
+
+        }
+
+
+
     }
 }
